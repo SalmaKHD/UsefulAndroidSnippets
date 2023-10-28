fbb5fe8dba5c2f774021c58aa6fc056be27cf209
SalmaKHD <khodaeisalma8@gmail.com>
2023-10-28 04:22:07 -0700

    Update coil-for-compose.kt
    improve coil.kt

--- coil-for-compose/coil-for-compose.kt
+++ coil-for-compose/coil-for-compose.kt
@@ -1,8 +1,7 @@
 
 /*
-Topic of Investigation: Simple composable for showing images fetched from the network. Alternatice library: Glide: Supports placeholders for different network states
+Topic of Investigation: Simple composable for showing images fetched from the network. Alternatice library: Glide but Coil is more stable
 */
-
 const val IMAGE_URL = "https://images.unsplash.com/photo-1628373383885-4be0bc0172fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80"
 @Composable
 fun ImageFetchedFromNetwork(
@@ -14,14 +13,25 @@ fun ImageFetchedFromNetwork(
             .background(Color.Gray),
         contentAlignment = Alignment.Center
     ) {
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f / 1f),
-            painter = rememberAsyncImagePainter(IMAGE_URL),
-            contentDescription = "sky_image",
-            contentScale = ContentScale.Fit
-        )
+        Column(
+            modifier = Modifier.padding(vertical = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally, 
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            AsyncImage(
+                modifier = Modifier.weight(1f),
+                model = IMAGE_URL,
+                contentDescription = null,
+                placeholder = painterResource(id = R.drawable.circle_24),
+                alignment = Alignment.Center
+            )
+            Image(
+                modifier = Modifier.weight(1f),
+                painter = rememberAsyncImagePainter(IMAGE_URL),
+                contentDescription = "sky_image",
+                contentScale = ContentScale.Fit
+            )
+        }
     }
 }
 
